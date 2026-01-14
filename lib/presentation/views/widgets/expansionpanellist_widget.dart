28452ecbd7de878a0803b43adccb00b43894f4a0
KongBopha <kongbophaa@gmail.com>
2026-01-14 16:55:55 +0700

    update expansion widget

--- lib/presentation/views/widgets/expansionpanellist_widget.dart
+++ lib/presentation/views/widgets/expansionpanellist_widget.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 
-class Item {
-  List <String> expandedValue;
-  List <String> imageUrl ;
-  String headerValue;
-  bool isExpanded;
+class SubItem {
+  final String title;
+  final String imageUrl;
+  bool isChecked;
 
-
-  Item({
-    required this.expandedValue,
-    required this.headerValue,
-    this.isExpanded = false,
-    this.imageUrl = const [],
-
-
+  SubItem({
+    required this.title,
+    required this.imageUrl,
+    this.isChecked = false,
   });
 }
 
-// List<Item> generateItems(int num) {
-//   return List<Item>.generate(num, (int index) {
-//     return Item(
-//       expandedValue: "Expanded Value $index",
-//       headerValue: "Header Value $index",
-//     );
-//   });
-// }
+class Item {
+  final String headerValue;
+  final String imageUrl;
+  final List<SubItem> subItems;
+
+  bool isExpanded;
+  bool isChecked;
+
+  Item({
+    required this.headerValue,
+    required this.imageUrl,
+    required this.subItems,
+    this.isExpanded = false,
+    this.isChecked = false,
+  });
+}
 
 class ExpansionpanellistWidget extends StatefulWidget {
   const ExpansionpanellistWidget({super.key});
@@ -38,53 +41,125 @@ class _ExpansionpanellistWidgetState
     extends State<ExpansionpanellistWidget> {
   final List<Item> _data = [
     Item(
-      headerValue: 'Women', 
-      expandedValue: ['Women','T-shirt', 'Crop top','Hoodie','Jeans','Skirt'],
-      imageUrl: ['https://i.pinimg.com/236x/20/61/f2/2061f2fbacdd9ad3e32cf9c3af02e028.jpg','']
-      ),
+      headerValue: 'Women',
+      imageUrl:
+          'https://i.pinimg.com/236x/20/61/f2/2061f2fbacdd9ad3e32cf9c3af02e028.jpg',
+      subItems: [
+        SubItem(
+          title: 'T-shirt',
+          imageUrl:
+              'https://images.unsplash.com/photo-1521572163474-6864f9cf17ab',
+        ),
+        SubItem(
+          title: 'Tank Tops',
+          imageUrl:
+              'https://images.unsplash.com/photo-1512436991641-6745cdb1723f',
+        ),
+        SubItem(
+          title: 'Polos',
+          imageUrl:
+              'https://images.unsplash.com/photo-1583743814966-8936f5b7be1a',
+        ),
+      ],
+    ),
     Item(
-      headerValue: 'Men', 
-      expandedValue: ['Men','T-shirt', 'Crop top','Hoodie','Jeans','Skirt'],
-      imageUrl: ['https://i.pinimg.com/236x/20/61/f2/2061f2fbacdd9ad3e32cf9c3af02e028.jpg','']
-      ),
-    Item(
-      headerValue: 'Kids', 
-      expandedValue: ['Kids','T-shirt', 'Crop top','Hoodie','Jeans','Skirt'],
-      imageUrl: ['https://i.pinimg.com/236x/20/61/f2/2061f2fbacdd9ad3e32cf9c3af02e028.jpg','']
-      ),
-    ];
-    bool isChecked = false;
+      headerValue: 'Men',
+      imageUrl:
+          'https://i.pinimg.com/originals/bd/ee/66/bdee662afda31d05b8175665bc6bc110.jpg',
+      subItems: [
+        SubItem(
+          title: 'Jackets',
+          imageUrl:
+              'https://images.unsplash.com/photo-1520975916090-3105956dac38',
+        ),
+        SubItem(
+          title: 'Hoodies',
+          imageUrl:
+              'https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f',
+        ),
+      ],
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: _buildPanel(),
+    return Scaffold(
+      body: ListView(
+        children: _data.map(_buildCategory).toList(),
+      ),
     );
   }
 
-  Widget _buildPanel() {
-    return ExpansionPanelList(
-      expansionCallback: (int index, bool isExpanded) {
-        setState(() {
-          _data[index].isExpanded = isExpanded;
-        });
-      },
-      children: _data.map<ExpansionPanel>((Item item) {
-        return ExpansionPanel(
-          isExpanded: item.isExpanded,
-          headerBuilder: (BuildContext context, bool isExpanded) {
-            return ListTile(
-              title: Text(item.headerValue),
-            );
+  Widget _buildCategory(Item item) {
+    return Column(
+      children: [
+        // ===== MAIN CATEGORY =====
+        InkWell(
+          onTap: () {
+            setState(() => item.isExpanded = !item.isExpanded);
           },
-          body: Column(
-            children: item.expandedValue.map((detail) => ListTile(
-              title: Text(detail),
-              leading: Icon(Icons.check_box_outline_blank),
-            )).toList(),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Row(
+              children: [
+                Checkbox(
+                  value: item.isChecked,
+                  onChanged: (v) {
+                    setState(() => item.isChecked = v ?? false);
+                  },
+                  checkColor: Colors.red,
+                  
+
+                ),
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(item.imageUrl),
+                ),
+                const SizedBox(width: 12),
+                Text(
+                  item.headerValue,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500,color: Colors.black87),
+                ),
+                const Spacer(),
+                Icon(item.isExpanded
+                    ? Icons.keyboard_arrow_up
+                    : Icons.keyboard_arrow_down),
+              ],
+            ),
           ),
-        );
-      }).toList(),
+        ),
+
+        // ===== SUB ITEMS =====
+        if (item.isExpanded)
+          Column(
+            children: item.subItems.map((sub) {
+              return Padding(
+                padding:
+                    const EdgeInsets.only(left: 50, right: 16, bottom: 10),
+                child: Row(
+                  children: [
+                    Checkbox(
+                      value: sub.isChecked,
+                      onChanged: (v) {
+                        setState(() => sub.isChecked = v ?? false);
+                      },
+                      checkColor: Colors.red,
+                    ),
+                    CircleAvatar(
+                      radius: 14,
+                      backgroundImage: NetworkImage(sub.imageUrl),
+                    ),
+                    const SizedBox(width: 12),
+                    Text(sub.title, style: const TextStyle(fontSize: 14,color: Colors.black87)),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+
+        const Divider(height: 1),
+      ],
     );
   }
 }
